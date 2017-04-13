b6b41259527fb0c0cfaa2f7dfd3abc70adda09df
aphramzo <im.t.wal@gmail.com>
2017-04-12 22:43:53 -0600

    Oops

--- src/AllScriptsTest/Controllers/HomeController.cs
+++ src/AllScriptsTest/Controllers/HomeController.cs
@@ -12,17 +12,7 @@ namespace AllScriptsTest.Controllers
     {
         public IActionResult Index()
         {
-            string clientId = "27bbad6d-5f76-474a-8c5e-baf68eb6b10c";
-            string secret = "DC94CFCE6213";
-            string scope = "patient/*.read";
-            string parameters = string.Format("grant_type=client_credentials&client_id={0}&client_secret={1}&scope={2}", clientId, secret, scope);
-
-            var client = new RestClient("https://tw171.open.allscripts.com/authorization/connect/token");
-
-            var request = new RestRequest(Method.POST); request.AddHeader("cache-control", "no-cache");
-
-            request.AddHeader("content-type", "application/x-www-form-urlencoded");
-            request.AddParameter("application/x-www-form-urlencoded", parameters, ParameterType.RequestBody);
+         
 
             return View();
         }
